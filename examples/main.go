d7178d761e84ed563c292c9bd92ebe14e78c114b
andreyev <andreyev@tutu.tech>
2023-04-26 07:11:13 +0300

    Fix logging

--- examples/main.go
+++ examples/main.go
@@ -68,9 +68,7 @@ l:
 						default:
 							fmt.Println("Peers:")
 							for _, p := range connectedPeers(db.GetHost()) {
-								//pubKey, _ := p.ID.ExtractPublicKey()
-								//eth, _ := p2p_database.GetEthAddrFromPeer(pubKey)
-								fmt.Printf("Peer [%s] %s %s\r\n", p.ID, p.Addrs[0].String())
+								logging.Logger("cli").Infof("Peer [%s] %s\r\n", p.ID, p.Addrs[0].String())
 							}
 							fmt.Println()
 							time.Sleep(3 * time.Second)
